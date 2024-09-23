0df6c59a8d2005e9c959dfce4c74b9ef8f1ad1a4
Phelix <phelixmbani@gmail.com>
2024-09-23 14:44:37 +0300

    string lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+let newcurrentUser = currentUser.toUpperCase()
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${newcurrentUser}!`
+let shortGreeting = "Welcome, "
+shortGreeting = "Welcome, " + "Grace Hopper"[0] +"!"
+console.log(shortGreeting)
